6fea451b45e25f1a4a6ab9f9ecd52e338e73273f
wzd <wzd@wzd2-MacBook-Pro.local>
2021-07-02 00:45:45 +0800

    http base

---
+++ http_base/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"gee"
+	"net/http"
+)
+
+type Engine struct{}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
+	case "/":
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	case "/hello":
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "qqqHeader[%q] = %q\n", k, v)
+		}
+	default:
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+	}
+}
+
+func main() {
+	//HTTP.hANDLEfUNC("/", INDEXhANDLER)
+	// http.HandleFunc("/hello", helloHandler)
+	// engine := new(Engine)
+	// log.Fatal(http.ListenAndServe(":9999", engine))
+	// fmt.Println("vim-go")
+	r := gee.New()
+	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "URL.Path=%q\n", req.URL.Path)
+	})
+	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	})
+	r.Run(":9999")
+}
+
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+}
+
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	for k, v := range req.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+}
