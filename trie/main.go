934956ce830f81e15f78c6b6a1d72eb3bfbe98ba
zandwang <wzd15216412363@gmail.com>
2021-08-02 01:40:55 +0800

    add context

---
+++ trie/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"gee"
+	"net/http"
+)
+
+type Engine struct{}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
+	case "/":
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	case "/hello":
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "qqqHeader[%q] = %q\n", k, v)
+		}
+	default:
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+	}
+}
+
+func main() {
+	//HTTP.hANDLEfUNC("/", INDEXhANDLER)
+	// http.HandleFunc("/hello", helloHandler)
+	// engine := new(Engine)
+	// log.Fatal(http.ListenAndServe(":9999", engine))
+	// fmt.Println("vim-go")
+	r := gee.New()
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+	r.GET("/hello", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
+	r.Run(":9999")
+}
+
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+}
+
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	for k, v := range req.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+}
